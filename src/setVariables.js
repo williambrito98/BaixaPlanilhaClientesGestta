3620a292869d8e1929d85a960c0294cc89d1c0e2
williambrito98 <williambrito199850@gmail.com>
2023-02-21 19:15:36 -0300

    chore: add pathLog in global variables

--- src/setVariables.js
+++ src/setVariables.js
@@ -3,9 +3,11 @@ const { existsSync, mkdirSync } = require('fs')
 const CONFIG = require('../config.json')
 
 module.exports = function setVariables () {
-  global.pathSaida = join(__dirname, 'download')
+  global.pathSaida = join(process.cwd(), 'download')
+  global.pathLog = join(process.cwd(), 'log')
 
   !existsSync(global.pathSaida) ? mkdirSync(global.pathSaida) : null
+  !existsSync(global.pathLog) ? mkdirSync(global.pathLog) : null
 
   global.pathExcelFile = join(global.pathSaida, CONFIG.FILE_NAME)
 }
