1e2a3a01866ac26d1ad07268d01a44e1c241a83e
williambrito98 <williambrito199850@gmail.com>
2023-02-21 19:15:17 -0300

    fix: handling data

--- src/report.js
+++ src/report.js
@@ -1,6 +1,6 @@
 const request = require('./utils/request')
 const CONFIG = require('../config.json')
-const { getYear, getLongMonth, getDay, getFullDate } = require('./utils/date')
+const { getYear, getLongMonth, getDay, getFullDate, getFullHours } = require('./utils/date')
 
 /**
  * @param {string} token
@@ -44,20 +44,20 @@ module.exports = {
   handlerData: function (data) {
     return data.map(item => ({
       'Cliente - Nome': item['customer.name'] ?? '',
-      'Empresa - Data de Criação.Ano': getYear(item?.['company.created']?.slice(0, 10)),
-      'Empresa - Data de Criação.Dia': getLongMonth(item?.['company.created']?.slice(0, 10)),
-      'Empresa - Data de Criação.Mês': getDay(item?.['company.created']?.slice(0, 10)),
-      'Tarefa - Data da entrega dos documentos solicitados.Ano': '',
-      'Tarefa - Data da entrega dos documentos solicitados.Dia': '',
-      'Tarefa - Data da entrega dos documentos solicitados.Mês': '',
-      'Tarefa - Data de conclusão.Ano': getYear(item?.conclusion_date?.slice(0, 10)),
-      'Tarefa - Data de conclusão.Dia': getDay(item?.conclusion_date?.slice(0, 10)),
-      'Tarefa - Data de conclusão.Mês': getLongMonth(item?.conclusion_date?.slice(0, 10)),
-      'Tarefa - Data de Criação.Ano': getYear(item?.created_at?.slice(0, 10)),
-      'Tarefa - Data de Criação.Dia': getDay(item?.created_at?.slice(0, 10)),
-      'Tarefa - Data de Criação.Mês': getLongMonth(item?.created_at?.slice(0, 10)),
-      'Tarefa - Data de Vencimento.Ano': getYear(item?.conclusion_date?.slice(0, 10)),
-      'Tarefa - Data de Vencimento.Dia': getDay(item?.conclusion_date?.slice(0, 10)),
+      'Empresa - Data de Criação.Ano': getYear(item?.['company.created']),
+      'Empresa - Data de Criação.Dia': getLongMonth(item?.['company.created']),
+      'Empresa - Data de Criação.Mês': getDay(item?.['company.created']),
+      'Tarefa - Data da entrega dos documentos solicitados.Ano': getYear(item?.['document_request.requested_documents_upload_date']),
+      'Tarefa - Data da entrega dos documentos solicitados.Dia': getDay(item?.['document_request.requested_documents_upload_date']),
+      'Tarefa - Data da entrega dos documentos solicitados.Mês': getLongMonth(item?.['document_request.requested_documents_upload_date']),
+      'Tarefa - Data de conclusão.Ano': getYear(item?.conclusion_date),
+      'Tarefa - Data de conclusão.Dia': getDay(item?.conclusion_date),
+      'Tarefa - Data de conclusão.Mês': getLongMonth(item?.conclusion_date),
+      'Tarefa - Data de Criação.Ano': getYear(item?.created_at),
+      'Tarefa - Data de Criação.Dia': getDay(item?.created_at),
+      'Tarefa - Data de Criação.Mês': getLongMonth(item?.created_at),
+      'Tarefa - Data de Vencimento.Ano': getYear(item?.conclusion_date),
+      'Tarefa - Data de Vencimento.Dia': getDay(item?.conclusion_date),
       'Cliente - Ativo?': item?.['customer.active'] ? 'Sim' : 'Não',
       'Cliente - CNPJ': item?.['customer.cnpj'] ?? '',
       'Cliente - Código': item?.['customer.code'] ?? '',
@@ -73,12 +73,12 @@ module.exports = {
       'Tarefa - Concluída em Atraso': item?.done_overdue ? 'Sim' : 'Não',
       'Tarefa - Concluído por': item?.['concluded_by.name'] ?? '',
       'Tarefa - Concluído por (Papel)': item?.['owner.role'] ?? '',
-      'Tarefa - Data da entrega dos documentos solicitados (completa)': '',
+      'Tarefa - Data da entrega dos documentos solicitados (completa)': getFullDate(item?.['document_request.requested_documents_upload_date']),
       'Tarefa - Data de conclusão (completa)': getFullDate(item?.conclusion_date),
       'Tarefa - Data de Vencimento (completa)': getFullDate(item?.conclusion_date),
-      'Tarefa - Desconsiderada para sempre?': '',
+      'Tarefa - Desconsiderada para sempre?': item?._forever ? 'Sim' : 'Não',
       'Tarefa - Gera Multa?': item?.fine ? 'Sim' : 'Não',
-      'Tarefa - Horário da entrega dos documentos solicitados': '',
+      'Tarefa - Horário da entrega dos documentos solicitados': getFullHours(item?.['document_request.requested_documents_upload_date']),
       'Tarefa - ID': item?.id ?? '',
       'Tarefa - No prazo?': item?.on_time ? 'Sim' : 'Não',
       'Tarefa - Nome': item?.name ?? '',
@@ -89,8 +89,8 @@ module.exports = {
       'Tarefa - Data de Vencimento.Mês': getLongMonth(item?.conclusion_date),
       'Empresa - Departamento': item?.['company_department.name'] ?? '',
       'Tarefa - Status': CONFIG.STATUS[item?.status] ?? '',
-      'Cliente - Dia da Mensalidade': '',
-      'Cliente - Valor da Mensalidade': '',
+      'Cliente - Dia da Mensalidade': item?.['customer.monthly_day_payment'] ?? '',
+      'Cliente - Valor da Mensalidade': item?.['customer.monthly_payment'] ?? '',
       'Tarefa - Pontuação': item?.['company_task.score'] ?? '',
       'Tarefa - Semana do Vencimento': item?.due_iso_week ?? '',
       'Tarefa - Valor': item?.value ?? ''
