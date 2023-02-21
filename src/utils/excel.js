6b99d0349e947644ac6ba006b51839e6f8bd5ae6
williambrito98 <williambrito199850@gmail.com>
2023-02-21 19:16:46 -0300

    feat: dinamic sheet name

--- src/utils/excel.js
+++ src/utils/excel.js
@@ -1,10 +1,15 @@
 const XLSX = require('xlsx')
 
 module.exports = {
-  jsonToXLSX: function (data) {
+  /**
+   *
+   * @param {string} data
+   * @param {string} sheetName
+   */
+  jsonToXLSX: function (data, sheetName) {
     const wb = XLSX.utils.book_new()
     const ws = XLSX.utils.json_to_sheet(data, { header: Object.keys(data[0]) })
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1')
+    XLSX.utils.book_append_sheet(wb, ws, sheetName)
 
     XLSX.writeFileXLSX(wb, global.pathExcelFile)
   }
